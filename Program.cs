9f99aaf12dba2134b4d3efe8adfafb74fa2fb26f
darklord <obed1355@gmail.com>
2020-01-23 23:12:15 -0400

    add project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Util_Repository_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
